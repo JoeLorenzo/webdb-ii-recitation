fd36d95080e17456e7475de80d25f3b526efab2a
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-01-10 23:49:05 -0600

    -Connected the database

---
+++ data/db.js
@@ -0,0 +1,8 @@
+const knex = require("knex")
+const knexConfig = require("../knexfile")
+
+// the .env is for heroku
+// const db = knex(knexConfig[process.env.NODE_ENV || "development" ])
+const db = knex(knexConfig"development")
+
+module.exports = db
