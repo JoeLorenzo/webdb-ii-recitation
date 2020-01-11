00d258243b3be2ad2baeec2c843f8ded023e5419
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-01-10 23:56:47 -0600

    -Exported db.js module.

--- data/db.js
+++ data/db.js
@@ -3,6 +3,6 @@ const knexConfig = require("../knexfile")
 
 // the .env is for heroku
 // const db = knex(knexConfig[process.env.NODE_ENV || "development" ])
-const db = knex(knexConfig"development")
+const db = knex(knexConfig.development)
 
 module.exports = db
