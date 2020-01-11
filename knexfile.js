8a532038e4365bbe0b399dfd18761feace32f326
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-01-10 23:31:07 -0600

    -Configured the knex file. -Created data directory. -Created the db file.

---
+++ knexfile.js
@@ -0,0 +1,52 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/books.db3'
+    },
+    migrations: {
+      directory: './data/migrations'
+  },
+    seeds: {
+      directory: './data/seeds'
+    }
+
+  // This code is not needed for the current project scope
+  // ,
+// 
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+};
